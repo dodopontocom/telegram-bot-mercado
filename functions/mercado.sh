b6812a6dc227ba28d6d834a01a238a637c5ca04a
Rodolfo Neto <rodolfotiago@gmail.com>
2020-03-05 14:34:56 -0300

    Update mercado.sh

--- functions/mercado.sh
+++ functions/mercado.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
-# 
+
+listar.compras(){
+        local item
+        item=$1
+        
+        botao_itens=''
+        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "✅" --callback_data 'item_comprado' --line 1
+        ShellBot.InlineKeyboardButton --button 'botao_itens' --text "preços 🔍" --callback_data 'item_valor' --line 1
+        keyboard_itens="$(ShellBot.InlineKeyboardMarkup -b 'botao_itens')"
+
+        ShellBot.deleteMessage --chat_id ${message_chat_id[$id]} --message_id ${message_message_id[$id]}
+        ShellBot.sendMessage    --chat_id ${message_chat_id[$id]} \
+                                --text "*${item}*" \
+                                --parse_mode markdown \
+                                --reply_markup "$keyboard_itens"
+}
+
+listar.apagar(){
+        
+        ShellBot.answerCallbackQuery --callback_query_id ${callback_query_id[$id]}
+        ShellBot.deleteMessage  --chat_id ${callback_query_message_chat_id[$id]} \
+                                --message_id ${callback_query_message_message_id[$id]}
+}
 
 # procura no site do tenda atacado e retorna o primeiro resultado do produto e preço
-
 product.search() {
   local product_name first_found product_price message
 
